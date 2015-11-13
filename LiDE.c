b9671383a7140256f489a2344399c8d1555da1ad
Alain Martel <dev@martel.srsly.ca>
2015-11-13 04:56:29 -0500

    Update LiDE.c

--- LiDE.c
+++ LiDE.c
@@ -8,7 +8,7 @@
 #include "LiDE.h"         // 
 //====Start-MD5-Support====
 #include <sys/stat.h>     // C data returned by the stat() function    --|
-#include <sys/mman.h>     // C memory management declarations            |== Support MD5 Functions
+#include <sys/mman.h>     // C memory management declarations            |== Support MD5 Function
 #include <fcntl.h>        // C file control options                      |
 #include <openssl/md5.h>  //                                           --|
 
@@ -31,22 +31,20 @@ unsigned long get_size_by_fd(int fd) {
 //====End-MD5-Support====
 
 void print_help(void){
-	printf("\x1B[33mLiDE (\033[1mLi\033[22mnux \033[1mD\033[22misk \033[1mE\033[22mxtractor) - v0.0.2.1\033[0m\n\n\
+	printf("\x1B[33mLiDE (\033[1mLi\033[22mnux \033[1mD\033[22misk \033[1mE\033[22mxtractor) - v0.0.3.0\033[0m\n\n\
 LiDE is a very small fileserver.\n\
-To host a file, do\n\n\
+Usage to host a file, do the following:\n\n\
 $ \x1B[32m./lide <filename>\033[0m              Will use default port 31337\n\
     or\n\
 $ \x1B[32m./lide -p <port> <filename>\033[0m    To specify a port to send on.\n\n\
 $ \x1B[32m./lide [-h]\033[0m                    Prints this help.\n\n\
 Then to retrieve it on another host, do something like\n\n\
-$ \x1B[32mnc <LiDE Host IP> 31337 > retrieved_filename\033[0m\n");
+$ \x1B[32mnetcat <LiDE Host IP> 31337 > retrieved_file && md5sum retrieved_file\033[0m\n");
 	exit(0);
 }
 
 int main(int argc, char *argv[])
 {
-
-	be_verbose = 1;
 	FILE *filename_fd;
 	char *filename_to_serve;
 	int listen_port = htons(31337);
@@ -106,5 +104,27 @@ int main(int argc, char *argv[])
 	}
 
 	if ((filename_fd = fopen(filename_to_serve, "r")) != NULL){
-	//====Start-MD5-Functions==== 
-	file_descript = open(filename_to_serve, O_RDONLY);
+		//====Start-MD5-Functions==== 
+		file_descript = open(filename_to_serve, O_RDONLY);
+		file_size = get_size_by_fd(file_descript);
+    
+		file_buffer = mmap(0, file_size, PROT_READ, MAP_SHARED, file_descript, 0);
+		MD5((unsigned char*) file_buffer, file_size, result);
+		munmap(file_buffer, file_size); 
+
+		printf("\x1B[32m");
+		print_md5_sum(result);
+		printf("  \t%s\t%lu\040bytes\033[0m\n", argv[argc-1], file_size);
+		//====End-MD5-Functions====
+
+		listen_for_inbound_requests(listen_port, filename_fd);		
+		printf("%d\n", listen_port);
+	}
+
+	else
+		perror("Error opening file");
+
+	free(filename_to_serve);
+
+	return 0;
+}
