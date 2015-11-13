f9e15672978cd56dfd8e3558882a6a404ad36f98
Alain Martel <dev@martel.srsly.ca>
2015-11-13 05:08:15 -0500

    Update LiDE.h

--- LiDE.h
+++ LiDE.h
@@ -1,5 +1,7 @@
 #define CHUNK_SIZE 1024
 char buffer[CHUNK_SIZE];
-
+//====Start-Local_IP_Address====
+char *ip_interface;
+//====End-Local_IP_Address====
 void print_help(void);
 char *read_file(FILE *filename_fd, int from);
