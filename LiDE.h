13a81025173fdef7117d36f5a958809dc02a845d
Alain Martel <dev@martel.srsly.ca>
2015-11-13 05:16:30 -0500

    Update LiDE.h

--- LiDE.h
+++ LiDE.h
@@ -1,11 +1,14 @@
+//====Start-Version====
+#define PROG_NAME       "LiDE"
+#define PROG_VERSION    "0.0.3.3"
+#define PROG_DATE       "2 June 2015"
+//====End-Version====
+
 #define CHUNK_SIZE 1024
 char buffer[CHUNK_SIZE];
-
 //====Start-Local_IP_Address====
 char *ip_interface;
 //====End-Local_IP_Address====
 
 void print_help(void);
 char *read_file(FILE *filename_fd, int from);
-
-char *LiDE_ver;
